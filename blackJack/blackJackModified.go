3c86abfc66479263553ffc6ca6b17afd608b36e7
cshoben <cshoben@users.noreply.github.com>
2022-04-04 21:25:06 -0400

    moved notes on official black jack game to appropriate file

--- blackJack/blackJackModified.go
+++ blackJack/blackJackModified.go
@@ -15,7 +15,7 @@ func welcomeMessage () {
 }
 
 // 	This is a variation of Black Jack, where the player is dealt all their cards, and
-// 	then the dealer is dealt theirs. The dealers "choice" to hit or stay is based on a random number generator. 
+// 	then the dealer is dealt theirs. The dealers "choice" to hit or stay is based on a random number generator.
 
 func cardValue(input string) int{
 	if strings.TrimRight(input, "\n") == "0" {return 0}
@@ -173,34 +173,4 @@ func playerTurn () int {
 			fmt.Println("You lose :(")
 		}
 	}
-
-
-
-//Goal: write a program so a player can play black jack via the terminal.
-
-/*
-Rules of black jack
-The player and the dealer receive two cards from a shuffled deck.
-In our case, we'll use a single deck, though casinos usually use a
-'shoe' consisting of six decks.
-
-After the first two cards are dealt to dealer and player, the player
-is asked if they'd like another card (called 'hitting'), or if they
-are happy with the cards they have already (called 'staying'). The
-object is to make the sum of your card values as close to 21, without
-going over. If we make 21 exactly, we have blackjack, which can't be beat.
-If we go over 21, we 'bust' and we lose the round. The player is allowed to
-stop hitting at any point.
-
-The number cards (2 through 10) are worth the number displayed, face
-cards are worth 10, and an Ace can be worth either 1 or 11. For example,
-if our first two cards are a Jack and an Ace, we'd want to count the Ace
-as 11 since 10 + 11 = 21, and we'd have blackjack, but, if we had
-already had a hand worth 18, decided to hit, and got an Ace, we'd
-want to count it as 1, since counting it as 11 would put us at 29, and we'd bust.
-
-Once our hand is finished, the dealer tries to do the same. The dealer must
-keep hitting until they get to 17. If they get above 17 without busting, they can stay.
-
-*/
 
